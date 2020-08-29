533974d8a4c3fe08e0af180efbb8e9aacc6eb30c
Andres Campuzano <acampuz1@gmail.com>
2020-08-29 17:59:06 -0500

    Added docs file

---
+++ docs/precache-manifest.25cc7f9fd7d446a837076fa6645bc6b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ad329b4aa560611fa1020caf5352a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c99b88a873b7425701",
+    "url": "/static/css/main.6393f400.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c99b88a873b7425701",
+    "url": "/static/js/main.4b84e394.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
